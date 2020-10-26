63b323ab25ca301eac874f63d046f96d67ca1d24
Dustin Morgan <ddmorgan92>
2020-10-26 16:53:29 -0500

    Updates

---
+++ precache-manifest.2fc5c10ac1e2f58d6b15eb0bc89e6fc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12100ab354446c96d838715bc4691fb",
+    "url": "/Weather-React/index.html"
+  },
+  {
+    "revision": "38c4f69d4472eb4d74dc",
+    "url": "/Weather-React/static/css/main.b996ab55.chunk.css"
+  },
+  {
+    "revision": "7b978fc9def7fb04d517",
+    "url": "/Weather-React/static/js/2.68d2bb24.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Weather-React/static/js/2.68d2bb24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38c4f69d4472eb4d74dc",
+    "url": "/Weather-React/static/js/main.2afc31fa.chunk.js"
+  },
+  {
+    "revision": "cbe44a6c52af65528d87",
+    "url": "/Weather-React/static/js/runtime-main.8a38df44.js"
+  },
+  {
+    "revision": "78646409f3ff5b086adf18368f798aad",
+    "url": "/Weather-React/static/media/cool-bg.78646409.jpg"
+  },
+  {
+    "revision": "1c69db2b5bf4591637a0e4bc88d34cc5",
+    "url": "/Weather-React/static/media/warm-bg.1c69db2b.jpg"
+  }
+]);
